a5cc65dbd22b44996ed03898ca560e5c3bad6818
Asif Ahmed Jesi <asifahmedjesi@gmail.com>
2020-09-01 00:07:39 +0600

    Add Recipe Application and CircleCI Config file

---
+++ src/main/java/com/a4aero/spring5recipecircleci/Spring5RecipeCircleciApplication.java
@@ -0,0 +1,13 @@
+package com.a4aero.spring5recipecircleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeCircleciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipeCircleciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a4aero/spring5recipecircleci/Spring5RecipeCircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a4aero.spring5recipecircleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeCircleciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
